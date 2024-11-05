3db946771b0bc170e91abcdfd49c5e89ee967334
sahiltambe18 <sahiltambe81922@gmail.com>
2024-11-06 01:57:45 +0530

    brain tumor

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 import tensorflow as tf
-print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
+
+# Create a random tensor with compatible shapes for multiplication
+a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])  # Shape (2, 3)
+b = tf.constant([[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]])  # Shape (3, 2)
+
+c = tf.matmul(a, b)  # Now this will work
+print("Result of matrix multiplication:", c.numpy())
+
